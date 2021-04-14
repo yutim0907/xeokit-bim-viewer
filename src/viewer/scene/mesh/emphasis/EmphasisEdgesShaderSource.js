5469b523a7ca4e934df1c9c6eed16229c9eff8f2
yutim0907 <yutim0907@gmail.com>
2021-04-14 11:28:04 +0800

    Initial commit

---
+++ src/viewer/scene/mesh/emphasis/EmphasisEdgesShaderSource.js
@@ -0,0 +1,163 @@
+import {WEBGL_INFO} from "../../webglInfo.js";
+
+/**
+ * @private
+ */
+class EmphasisEdgesShaderSource {
+    constructor(mesh) {
+        this.vertex = buildVertex(mesh);
+        this.fragment = buildFragment(mesh);
+    }
+}
+
+function buildVertex(mesh) {
+    const scene = mesh.scene;
+    const clipping = scene._sectionPlanesState.sectionPlanes.length > 0;
+    const quantizedGeometry = !!mesh._geometry._state.compressGeometry;
+    const billboard = mesh._state.billboard;
+    const stationary = mesh._state.stationary;
+    const src = [];
+    src.push("// Edges drawing vertex shader");
+    if (scene.logarithmicDepthBufferEnabled && WEBGL_INFO.SUPPORTED_EXTENSIONS["EXT_frag_depth"]) {
+        src.push("#extension GL_EXT_frag_depth : enable");
+    }
+    src.push("attribute vec3 position;");
+    src.push("uniform mat4 modelMatrix;");
+    src.push("uniform mat4 viewMatrix;");
+    src.push("uniform mat4 projMatrix;");
+    src.push("uniform vec4 edgeColor;");
+    src.push("uniform vec3 offset;");
+    if (quantizedGeometry) {
+        src.push("uniform mat4 positionsDecodeMatrix;");
+    }
+    if (scene.logarithmicDepthBufferEnabled) {
+        src.push("uniform float logDepthBufFC;");
+        if (WEBGL_INFO.SUPPORTED_EXTENSIONS["EXT_frag_depth"]) {
+            src.push("varying float vFragDepth;");
+        }
+    }
+    if (clipping) {
+        src.push("varying vec4 vWorldPosition;");
+    }
+    src.push("varying vec4 vColor;");
+    if (billboard === "spherical" || billboard === "cylindrical") {
+        src.push("void billboard(inout mat4 mat) {");
+        src.push("   mat[0][0] = 1.0;");
+        src.push("   mat[0][1] = 0.0;");
+        src.push("   mat[0][2] = 0.0;");
+        if (billboard === "spherical") {
+            src.push("   mat[1][0] = 0.0;");
+            src.push("   mat[1][1] = 1.0;");
+            src.push("   mat[1][2] = 0.0;");
+        }
+        src.push("   mat[2][0] = 0.0;");
+        src.push("   mat[2][1] = 0.0;");
+        src.push("   mat[2][2] =1.0;");
+        src.push("}");
+    }
+    src.push("void main(void) {");
+    src.push("vec4 localPosition = vec4(position, 1.0); ");
+    src.push("vec4 worldPosition;");
+    if (quantizedGeometry) {
+        src.push("localPosition = positionsDecodeMatrix * localPosition;");
+    }
+    src.push("mat4 viewMatrix2 = viewMatrix;");
+    src.push("mat4 modelMatrix2 = modelMatrix;");
+    if (stationary) {
+        src.push("viewMatrix2[3][0] = viewMatrix2[3][1] = viewMatrix2[3][2] = 0.0;")
+    }
+    if (billboard === "spherical" || billboard === "cylindrical") {
+        src.push("mat4 modelViewMatrix = viewMatrix2 * modelMatrix2;");
+        src.push("billboard(modelMatrix2);");
+        src.push("billboard(viewMatrix2);");
+        src.push("billboard(modelViewMatrix);");
+        src.push("worldPosition = modelMatrix2 * localPosition;");
+        src.push("worldPosition.xyz = worldPosition.xyz + offset;");
+        src.push("vec4 viewPosition = modelViewMatrix * localPosition;");
+    } else {
+        src.push("worldPosition = modelMatrix2 * localPosition;");
+        src.push("worldPosition.xyz = worldPosition.xyz + offset;");
+        src.push("vec4 viewPosition  = viewMatrix2 * worldPosition; ");
+    }
+    src.push("vColor = edgeColor;");
+    if (clipping) {
+        src.push("vWorldPosition = worldPosition;");
+    }
+    src.push("vec4 clipPos = projMatrix * viewPosition;");
+    if (scene.logarithmicDepthBufferEnabled) {
+        if (WEBGL_INFO.SUPPORTED_EXTENSIONS["EXT_frag_depth"]) {
+            src.push("vFragDepth = 1.0 + clipPos.w;");
+        } else {
+            src.push("clipPos.z = log2( max( 1e-6, clipPos.w + 1.0 ) ) * logDepthBufFC - 1.0;");
+            src.push("clipPos.z *= clipPos.w;");
+        }
+    }
+    src.push("gl_Position = clipPos;");
+    src.push("}");
+    return src;
+}
+
+function buildFragment(mesh) {
+
+    const scene = mesh.scene;
+    const sectionPlanesState = mesh.scene._sectionPlanesState;
+    const gammaOutput = mesh.scene.gammaOutput;
+    const clipping = sectionPlanesState.sectionPlanes.length > 0;
+    const src = [];
+
+    src.push("// Edges drawing fragment shader");
+    if (scene.logarithmicDepthBufferEnabled && WEBGL_INFO.SUPPORTED_EXTENSIONS["EXT_frag_depth"]) {
+        src.push("#extension GL_EXT_frag_depth : enable");
+    }
+    src.push("#ifdef GL_FRAGMENT_PRECISION_HIGH");
+    src.push("precision highp float;");
+    src.push("precision highp int;");
+    src.push("#else");
+    src.push("precision mediump float;");
+    src.push("precision mediump int;");
+    src.push("#endif");
+    if (scene.logarithmicDepthBufferEnabled && WEBGL_INFO.SUPPORTED_EXTENSIONS["EXT_frag_depth"]) {
+        src.push("uniform float logDepthBufFC;");
+        src.push("varying float vFragDepth;");
+    }
+    if (gammaOutput) {
+        src.push("uniform float gammaFactor;");
+        src.push("vec4 linearToGamma( in vec4 value, in float gammaFactor ) {");
+        src.push("  return vec4( pow( value.xyz, vec3( 1.0 / gammaFactor ) ), value.w );");
+        src.push("}");
+    }
+    if (clipping) {
+        src.push("varying vec4 vWorldPosition;");
+        src.push("uniform bool clippable;");
+        for (let i = 0, len = sectionPlanesState.sectionPlanes.length; i < len; i++) {
+            src.push("uniform bool sectionPlaneActive" + i + ";");
+            src.push("uniform vec3 sectionPlanePos" + i + ";");
+            src.push("uniform vec3 sectionPlaneDir" + i + ";");
+        }
+    }
+    src.push("varying vec4 vColor;");
+    src.push("void main(void) {");
+    if (clipping) {
+        src.push("if (clippable) {");
+        src.push("  float dist = 0.0;");
+        for (let i = 0, len = sectionPlanesState.sectionPlanes.length; i < len; i++) {
+            src.push("if (sectionPlaneActive" + i + ") {");
+            src.push("   dist += clamp(dot(-sectionPlaneDir" + i + ".xyz, vWorldPosition.xyz - sectionPlanePos" + i + ".xyz), 0.0, 1000.0);");
+            src.push("}");
+        }
+        src.push("  if (dist > 0.0) { discard; }");
+        src.push("}");
+    }
+    if (scene.logarithmicDepthBufferEnabled && WEBGL_INFO.SUPPORTED_EXTENSIONS["EXT_frag_depth"]) {
+        src.push("gl_FragDepthEXT = log2( vFragDepth ) * logDepthBufFC * 0.5;");
+    }
+    if (gammaOutput) {
+        src.push("gl_FragColor = linearToGamma(vColor, gammaFactor);");
+    } else {
+        src.push("gl_FragColor = vColor;");
+    }
+    src.push("}");
+    return src;
+}
+
+export {EmphasisEdgesShaderSource};
---
+++ src/viewer/scene/mesh/pick/PickMeshShaderSource.js
@@ -0,0 +1,146 @@
+/**
+ * @author xeolabs / https://github.com/xeolabs
+ */
+
+import {WEBGL_INFO} from "../../webglInfo.js";
+
+/**
+ * @private
+ */
+class PickMeshShaderSource {
+    constructor(mesh) {
+        this.vertex = buildVertex(mesh);
+        this.fragment = buildFragment(mesh);
+    }
+}
+
+function buildVertex(mesh) {
+    const scene = mesh.scene;
+    const clipping = scene._sectionPlanesState.sectionPlanes.length > 0;
+    const quantizedGeometry = !!mesh._geometry._state.compressGeometry;
+    const billboard = mesh._state.billboard;
+    const stationary = mesh._state.stationary;
+    const src = [];
+    src.push("// Mesh picking vertex shader");
+    if (scene.logarithmicDepthBufferEnabled && WEBGL_INFO.SUPPORTED_EXTENSIONS["EXT_frag_depth"]) {
+        src.push("#extension GL_EXT_frag_depth : enable");
+    }
+    src.push("attribute vec3 position;");
+    src.push("uniform mat4 modelMatrix;");
+    src.push("uniform mat4 viewMatrix;");
+    src.push("uniform mat4 projMatrix;");
+    src.push("varying vec4 vViewPosition;");
+    src.push("uniform vec3 offset;");
+    if (quantizedGeometry) {
+        src.push("uniform mat4 positionsDecodeMatrix;");
+    }
+    if (clipping) {
+        src.push("varying vec4 vWorldPosition;");
+    }
+    if (scene.logarithmicDepthBufferEnabled) {
+        src.push("uniform float logDepthBufFC;");
+        if (WEBGL_INFO.SUPPORTED_EXTENSIONS["EXT_frag_depth"]) {
+            src.push("varying float vFragDepth;");
+        }
+    }
+    if (billboard === "spherical" || billboard === "cylindrical") {
+        src.push("void billboard(inout mat4 mat) {");
+        src.push("   mat[0][0] = 1.0;");
+        src.push("   mat[0][1] = 0.0;");
+        src.push("   mat[0][2] = 0.0;");
+        if (billboard === "spherical") {
+            src.push("   mat[1][0] = 0.0;");
+            src.push("   mat[1][1] = 1.0;");
+            src.push("   mat[1][2] = 0.0;");
+        }
+        src.push("   mat[2][0] = 0.0;");
+        src.push("   mat[2][1] = 0.0;");
+        src.push("   mat[2][2] =1.0;");
+        src.push("}");
+    }
+    src.push("void main(void) {");
+    src.push("vec4 localPosition = vec4(position, 1.0); ");
+    if (quantizedGeometry) {
+        src.push("localPosition = positionsDecodeMatrix * localPosition;");
+    }
+    src.push("mat4 viewMatrix2 = viewMatrix;");
+    src.push("mat4 modelMatrix2 = modelMatrix;");
+    if (stationary) {
+        src.push("viewMatrix2[3][0] = viewMatrix2[3][1] = viewMatrix2[3][2] = 0.0;")
+    }
+    if (billboard === "spherical" || billboard === "cylindrical") {
+        src.push("mat4 modelViewMatrix = viewMatrix2 * modelMatrix2;");
+        src.push("billboard(modelMatrix2);");
+        src.push("billboard(viewMatrix2);");
+    }
+    src.push("   vec4 worldPosition = modelMatrix2 * localPosition;");
+    src.push("   worldPosition.xyz = worldPosition.xyz + offset;");
+    src.push("   vec4 viewPosition = viewMatrix2 * worldPosition;");
+    if (clipping) {
+        src.push("   vWorldPosition = worldPosition;");
+    }
+    src.push("vec4 clipPos = projMatrix * viewPosition;");
+    if (scene.logarithmicDepthBufferEnabled) {
+        if (WEBGL_INFO.SUPPORTED_EXTENSIONS["EXT_frag_depth"]) {
+            src.push("vFragDepth = 1.0 + clipPos.w;");
+        } else {
+            src.push("clipPos.z = log2( max( 1e-6, clipPos.w + 1.0 ) ) * logDepthBufFC - 1.0;");
+            src.push("clipPos.z *= clipPos.w;");
+        }
+    }
+    src.push("gl_Position = clipPos;");
+    src.push("}");
+    return src;
+}
+
+function buildFragment(mesh) {
+    const scene = mesh.scene;
+    const sectionPlanesState = scene._sectionPlanesState;
+    const clipping = sectionPlanesState.sectionPlanes.length > 0;
+    const src = [];
+    src.push("// Mesh picking fragment shader");
+    if (scene.logarithmicDepthBufferEnabled && WEBGL_INFO.SUPPORTED_EXTENSIONS["EXT_frag_depth"]) {
+        src.push("#extension GL_EXT_frag_depth : enable");
+    }
+    src.push("#ifdef GL_FRAGMENT_PRECISION_HIGH");
+    src.push("precision highp float;");
+    src.push("precision highp int;");
+    src.push("#else");
+    src.push("precision mediump float;");
+    src.push("precision mediump int;");
+    src.push("#endif");
+    if (scene.logarithmicDepthBufferEnabled && WEBGL_INFO.SUPPORTED_EXTENSIONS["EXT_frag_depth"]) {
+        src.push("uniform float logDepthBufFC;");
+        src.push("varying float vFragDepth;");
+    }
+    src.push("uniform vec4 pickColor;");
+    if (clipping) {
+        src.push("uniform bool clippable;");
+        src.push("varying vec4 vWorldPosition;");
+        for (var i = 0; i < sectionPlanesState.sectionPlanes.length; i++) {
+            src.push("uniform bool sectionPlaneActive" + i + ";");
+            src.push("uniform vec3 sectionPlanePos" + i + ";");
+            src.push("uniform vec3 sectionPlaneDir" + i + ";");
+        }
+    }
+    src.push("void main(void) {");
+    if (clipping) {
+        src.push("if (clippable) {");
+        src.push("  float dist = 0.0;");
+        for (var i = 0; i < sectionPlanesState.sectionPlanes.length; i++) {
+            src.push("if (sectionPlaneActive" + i + ") {");
+            src.push("   dist += clamp(dot(-sectionPlaneDir" + i + ".xyz, vWorldPosition.xyz - sectionPlanePos" + i + ".xyz), 0.0, 1000.0);");
+            src.push("}");
+        }
+        src.push("  if (dist > 0.0) { discard; }");
+        src.push("}");
+    }
+    if (scene.logarithmicDepthBufferEnabled && WEBGL_INFO.SUPPORTED_EXTENSIONS["EXT_frag_depth"]) {
+        src.push("gl_FragDepthEXT = log2( vFragDepth ) * logDepthBufFC * 0.5;");
+    }
+    src.push("   gl_FragColor = pickColor; ");
+    src.push("}");
+    return src;
+}
+
+export {PickMeshShaderSource};
