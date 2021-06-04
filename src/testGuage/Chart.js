56610c6332d2ac3824386dc1791a62a9effe667b
yutim0907 <yutim0907@gmail.com>
2021-06-04 16:35:38 +0800

    CHART

--- src/testGuage/Chart.js
+++ src/testGuage/Chart.js
@@ -1,40 +1,65 @@
-var fruitCanvas = document.getElementById("pieChart");
+var ctx = document.getElementById("barChart");
+var pieCtx = document.getElementById("pieChart");
 
-var fruitData = {
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+var pieData = {
     labels: [
-        "Mango",
-        "Apple",
-        "Grape",
-        "Pineapple",
-        "Watermelon"
+        "Red",
+        "Blue",
+        "Yellow"
     ],
     datasets: [
         {
-            data: [123, 75, 41, 61, 30],
+            data: [30, 50, 10],
             backgroundColor: [
-     "#fd7c78",
-    "#70dafc",
-    "#fed085",
-    "#b9e88b",
-    "#82a5fc",
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ],
+            hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
             ]
         }]
 };
-
-var options = {
-  title: {
-    display: true,
-    text: "Highlight a portion on click",
-    position: "top"
-  },
-  onClick: (evt, item) => {
-    pieChart.update();
-    item[0]._model.outerRadius += 10;
-  }
-};
-
-var pieChart = new Chart(fruitCanvas, {
-  type: 'pie',
-  data: fruitData,
-  options: options
+var myPieChart = new Chart(pieCtx,{
+    type: 'pie',
+    data: pieData,
+    //options: options
 });
